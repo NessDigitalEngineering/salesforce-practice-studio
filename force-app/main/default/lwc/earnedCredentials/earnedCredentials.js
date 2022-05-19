d0587b4a1597dc93f0a41f413b382e812be4cfd1
Ravichandraness <105056675+Ravichandraness@users.noreply.github.com>
2022-05-19 07:55:24 +0530

    Commiting enrnedCredential component

--- force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
+++ force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
@@ -1,13 +1,7 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getUserCredentials from '@salesforce/apex/earnedCredentialsController.getUserCredentials';
-
 import strUserId from '@salesforce/user/Id';
 
-const FIELDS = [
-    'User_Credential__c.Name',
-    'User_Credential__c.Badge_Icon__c',
-    'User_Credential__c.Status__c'
-];
 
 export default class EarnedCredentials extends LightningElement {
 userIds = strUserId;
@@ -16,16 +10,14 @@ userCredentialsData;
 userCredData;
 @track countRec;
 @track showMore = false;
-//@track NoshowMore = false;
 @track showMre =  false;
-//@track iconNm = 'standard:user';
+@track initialRecords = false;
+@track showMoreRecords = false;
 
 @wire(getUserCredentials, {userId:'$userIds'}) userdata({data,error}){
     if(data){
-        
-        this.countRec = data.length;
+       this.countRec = data.length;
        let selectedRec = [];
-
        if(this.countRec > 3){
             this.showMore = true;
         for(let i=0;i<this.countRec;i++){
@@ -33,13 +25,14 @@ userCredData;
                 selectedRec.push(data[i]);
             }
         }
-       // console.log("selectedRec" + JSON.stringify(selectedRec));
+                             this.initialRecords = true;
         this.userCredentialsData = selectedRec;
        }else{
+                             this.initialRecords = true;
         this.userCredentialsData = data;
        }
         
-        //console.log("data :"  + JSON.stringify(data));
+        
         this.title = 'Earned Credentials (' + data.length + ')';
 
     }else if(error)
@@ -49,10 +42,12 @@ userCredData;
     }
 }
 
-showMoreRecords(event){
-    //alert("inside show" );
-    //console.log("selectedRec" + JSON.stringify(userCredentialsData));
-   this.querySelector('c-earnedCredentialsModalPopUp');
-
+showMoreRec(){
+    getUserCredentials({userId:'$userIds'}).then(response=>{
+                             this.initialRecords = false;
+                             this.showMoreRecords = true;
+                             this.showMore = false;
+                             this.userCredentialsData = response;
+              }).catch(error=>{});
 }
 }
