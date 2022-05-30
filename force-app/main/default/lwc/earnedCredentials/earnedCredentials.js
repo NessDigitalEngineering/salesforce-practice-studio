07e66754323858e2c6513f19e54b673cd4dbf49e
Ravichandraness <105056675+Ravichandraness@users.noreply.github.com>
2022-05-30 18:29:14 +0530

    Commiting Component

---
+++ force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
@@ -0,0 +1,55 @@
+import { LightningElement, wire, track } from 'lwc';
+import getCompletedUserCredentials from '@salesforce/apex/UserCredentialService.getCompletedUserCredentials';
+import strUserId from '@salesforce/user/Id';
+
+
+export default class EarnedCredentials extends LightningElement {
+userIds = strUserId;
+title;
+userCredentialsData;
+userCredData;
+@track countRec;
+@track showMore = false;
+@track showMre =  false;
+@track initialRecords = false;
+@track showMoreRecords = false;
+
+@wire(getCompletedUserCredentials, {userId:'$userIds'}) userdata({data,error}){
+    if(data){
+       this.countRec = data.length;
+       let selectedRec = [];
+       if(this.countRec > 2){
+            this.showMore = true;
+        for(let i=0;i<this.countRec;i++){
+            if(i<2){
+                selectedRec.push(data[i]);
+            }
+        }
+                             this.initialRecords = true;
+        this.userCredentialsData = selectedRec;
+       }else{
+                             this.initialRecords = true;
+        this.userCredentialsData = data;
+       }
+        
+        
+        this.title = 'Earned Credentials (' + data.length + ')';
+
+    }else if(error)
+    {
+            console.log("error" + error);
+
+    }
+}
+
+showMoreRec(){
+    getCompletedUserCredentials({userId:this.userIds}).then(response=>{
+                             this.initialRecords = false;
+                             this.showMoreRecords = true;
+                             this.showMore = false;
+                             this.userCredentialsData = response;
+              }).catch(error=>{
+                  console.log("error" +error);
+              });
+}
+}
