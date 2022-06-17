10214bdb0cf7d61a27d33fe2cef9c62e42713246
vidhyabalasubramaniamness <vidhya.balasubramaniam@ness.com>
2022-06-17 18:45:08 +0530

    Voucher Request confirmation pop up

---
+++ force-app/main/default/lwc/voucherRequest/voucherRequest.js
@@ -0,0 +1,21 @@
+import { LightningElement, track } from 'lwc';
+
+export default class VoucherRequest extends LightningElement {
+    @track isShowModal = false;
+    @track showComponent = false;
+       userCredentials = [
+        {
+            Id: 1,
+            name :'Salesforce CPQ',
+            status : 'Assigned',
+            nextStatus: 'Ready'
+        }
+];
+voucherRequestModal(){
+    this.isShowModal=true;
+}
+
+hideModalBox() {  
+    this.isShowModal = false;
+}
+}
