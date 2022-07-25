309cee371852276705907dd6e1b0efebaa73bdf6
Nitin Savanur <102808229+nitinsavanurness@users.noreply.github.com>
2022-07-25 17:47:00 +0530

    Creating a copy of develop branch (#90)

---
+++ force-app/main/default/lwc/credentialAssignment/credentialAssignment.js
@@ -0,0 +1,315 @@
+import { api, LightningElement, track } from "lwc";
+import getUserCredentials from "@salesforce/apex/CredentialAssignmentController.getUserCredentials";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import insertCredAssignments from "@salesforce/apex/CredentialAssignmentController.insertCredAssignments";
+import { loadStyle } from "lightning/platformResourceLoader";
+import REMOVEROW from "@salesforce/resourceUrl/removeRow";
+import TasksIcon from "@salesforce/resourceUrl/TasksIcon";
+import headerTitle from "@salesforce/label/c.headerTitle";
+const cols = [
+  {
+    type: "button-icon",
+    fixedWidth: 35,
+    typeAttributes: {
+      iconName: "utility:delete",
+      name: "deleteIcon",
+      disabled: { fieldName: "deleteIcon" },
+    },
+  },
+  {
+    label: "Credential Name",
+    fieldName: "credName",
+    type: "text",
+    hideDefaultActions: "true",
+    wrapText: true,
+    cellAttributes: { class: "slds-text-color_default" },
+  },
+  {
+    label: "Assigned Date",
+    fieldName: "assignedDate",
+    type: "date-local",
+    editable: false,
+    typeAttributes: {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    },
+  },
+  {
+    label: "Due Date",
+    fieldName: "dueData",
+    sortable: "true",
+    type: "date-local",
+    editable: { fieldName: "controlEditField" },
+    typeAttributes: {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    },
+    cellAttributes:{ class: "fa fa-pencil" },
+    
+  },
+  {
+    label: "Status",
+    fieldName: "status",
+    type: "text",
+    hideDefaultActions: "true",
+  },
+];
+
+export default class CredentialAssignment extends LightningElement {
+  @api constant = {
+    ERROR_TITLE: "Error Found",
+    ROWACTION_DELETE: "deleteIcon",
+    VAR_SUCCESS: "Success",
+    ROWACTION_SAVE: "save",
+    VAR_ERROR: "error",
+    MSG_DELETE: "Record Deleted",
+    MSG_ERR_DEL: "Error deleting record",
+    MSG_ERR_UPD_RELOAD: "Error updating or reloading record",
+    MSG_UPD: "Credential(s) Successfully Assigned.",
+    DEL_CONFIRM: "Are you sure you want to delete this item ?",
+    MODE_BATCH: "batch",
+    MODE_SINGLE: "single",
+  };
+  @track columns;
+  @api removeCredentials;
+  @api selectedUserName = "";
+  @track credentials;
+  @track selectedCredentials;
+  @track isDataAvaialable = false;
+  @track saveButtonHide = false;
+  draftValues = [];
+  headerTitle = headerTitle;
+  title = 'List of Assignments';
+  wiredRecords;
+  refreshTable;
+  myMap = {};
+  @track sortBy;
+  @track sortDirection;
+  @track disableButton = false;
+  icon = TasksIcon;
+  @track showIcon = true;
+  @track isDialogVisible = false;
+  @track deleteRow;
+  confirmMessage= 'Are you sure you want to delete this item ?';
+  conf = 'Confirmation';
+  defaultMessage = 'Nothing needs your attention right now. Check back later.';
+  handleUserName(event) {
+    this.selectedUserName = event.detail.currentRecId;
+    this.handleCredential(event);
+  }
+
+  handleCredential(event) {
+    this.disableButton = true;
+    this.columns = cols;
+    let tempSelectRecords = [];
+    this.myMap = {};
+    if (this.selectedUserName && event.detail.selRecords) {
+      for (const rec of event.detail.selRecords) {
+        tempSelectRecords.push(rec.recId);
+        this.myMap[rec.recId] = rec.recName;
+      }
+      this.selectedCredentials = tempSelectRecords;
+      this.isDataAvaialable = true;
+      this.showIcon = false;
+      
+    } else {
+      this.template.querySelector("c-Credential-Search").resetCredentials();
+      this.selectedCredentials = tempSelectRecords;
+      
+      this.isDataAvaialable = false;
+      this.saveButtonHide = false;
+      this.showIcon = true;
+      
+    }
+
+    if (this.selectedUserName != "") {
+      this.getdata();
+    }
+  }
+  handleDelete(event) {
+    this.isDialogVisible = true;
+    this.deleteRow = event;
+
+  }
+  deleteRecord(){
+    try {
+      const action = this.deleteRow.detail.action.name;
+      //check for save or delete action
+
+      if (action != "" && action == this.constant.ROWACTION_DELETE) {
+        this.rowactionDelete(this.deleteRow);
+        }
+    } catch (errorMsg) {
+      console.log("error msg" + errorMsg);
+    }
+  }
+  rowactionDelete(event) {
+    //confirm to delete & invoke deleteRecord()
+   if (this.constant.DEL_CONFIRM) {
+      for (let cred in this.credentials) {
+        if (this.credentials[cred].credName === event.detail.row.credName && 
+          this.credentials[cred].status == "" ) {
+           this.template
+            .querySelector("c-Credential-Search")
+            .removeCredentials(this.credentials[cred].credName);
+          this.credentials.splice(cred, 1);
+          
+        }
+      }
+
+      let tempResponse = [];
+      let tempObject = {};
+      this.disableButton = true;
+      this.myMap = {};
+        for (const res of this.credentials) {
+          tempObject = { ...res };
+          tempResponse.push(tempObject);
+          tempObject = {};
+        if(res.status == ""){
+            this.saveButtonHide = true;
+            this.disableButton = false;
+          }
+          
+        }
+      this.isDialogVisible = false;
+      this.credentials = tempResponse;
+      this.removeCredentials = this.credentials;
+      if (this.credentials.length == 0) {
+          this.isDataAvaialable = false;
+          this.showIcon = true;
+          
+      }
+    }
+  }
+  renderedCallback() {
+    Promise.all([
+      // loadStyle( this, REMOVEROW)
+
+      loadStyle(this, REMOVEROW),
+    ])
+      .then(() => {
+        console.log("Files loaded");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+  getdata() {
+    getUserCredentials({ credmap: this.myMap, userId: this.selectedUserName })
+      .then((response) => {
+        let tempResponse = [];
+        let tempObject = {};
+
+        if (response) {
+          for (const res of response) {
+            tempObject = { ...res };
+
+            if (res.status == "") {
+              tempObject.controlEditField = true;
+              tempObject.deleteIcon = false;
+              this.saveButtonHide = true;
+              this.disableButton = false;
+            } else {
+              tempObject.controlEditField = false;
+              tempObject.deleteIcon = true;
+
+              this.saveButtonHide = true;
+              this.disableButton = true;
+            }
+
+            tempResponse.unshift(tempObject);
+          }
+        }
+
+        this.credentials = tempResponse;
+        if (this.credentials.length > 0) {
+          this.isDataAvaialable = true;
+          this.showIcon = false;
+        } else {
+          this.isDataAvaialable = false;
+          this.showIcon = true;
+         }
+      })
+      .catch((error) => {
+        console.log("error msg", error);
+      });
+  }
+
+  handleClick(event) {
+    insertCredAssignments({ credAssignmentList: this.credentials })
+      .then((_result) => {
+        let tempResponse = [];
+        let tempObject = {};
+        for (const res of this.credentials) {
+              tempObject = { ...res };
+
+            if (res.status == "") {
+              res.status = 'Assigned';
+              tempObject = { ...res };
+              tempResponse.push(tempObject);
+              tempObject.controlEditField = false;
+              tempObject.deleteIcon = true;
+
+              this.saveButtonHide = true;
+              this.disableButton = true;
+            } else {
+              tempResponse.push(tempObject);
+              tempObject.controlEditField = false;
+              tempObject.deleteIcon = true;
+
+              this.saveButtonHide = true;
+              this.disableButton = true;
+            }
+
+          }
+        
+        this.credentials = tempResponse;
+        this.dispatchEvent(
+          new ShowToastEvent({
+           message: this.constant.MSG_UPD,
+
+            variant: this.constant.VAR_SUCCESS,
+          })
+        );
+
+     this.template.querySelector("c-Credential-Search").resetCredentials();
+
+     this.disableButton = true;
+     })
+
+      .catch((error) => {
+        console.log("Errorured:- " + error.body.message);
+      });
+
+    this.draftValues = event.detail.draftValues;
+  }
+  handleSortCaseData(event) {
+    this.sortBy = event.detail.fieldName;
+    this.sortDirection = event.detail.sortDirection;
+    this.sortCaseData(this.sortBy, this.sortDirection);
+  }
+
+  sortCaseData(fieldname, direction) {
+    let parseData = JSON.parse(JSON.stringify(this.credentials));
+    // Return the value stored in the field
+    let keyValue = (a) => {
+      return a[fieldname];
+    };
+    // cheking reverse direction
+    let isReverse = direction === "asc" ? 1 : -1;
+    // sorting data
+    parseData.sort((x, y) => {
+      x = keyValue(x) ? keyValue(x) : ""; // handling null values
+      y = keyValue(y) ? keyValue(y) : "";
+      // sorting values based on direction
+      return isReverse * ((x > y) - (y > x));
+    });
+    this.credentials = parseData;
+  }
+  closeModal(){
+    this.isDialogVisible = false;
+}
+
+}
---
+++ force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
@@ -0,0 +1,62 @@
+import { LightningElement, wire, track, api } from "lwc";
+import getCompletedUserCredentials from "@salesforce/apex/UserCredentialService.getCompletedUserCredentials";
+import strUserId from "@salesforce/user/Id";
+export default class EarnedCredentials extends LightningElement {
+  userIds = strUserId;
+  title;
+  userCredentialsData;
+  userCredData;
+  @track countRec;
+  @track showMore = false;
+  @track showMre = false;
+  @track initialRecords = false;
+  @track showMoreRecords = false;
+  @track loaded = false;
+  @track totalUserCredentials;
+  @api senddata = "";
+
+  @wire(getCompletedUserCredentials, { userId: "$senddata" }) userdata({
+    data,
+    error,
+  }) {
+    if (data) {
+      this.countRec = data.length;
+      let selectedRec = [];
+      if (this.countRec > 2) {
+        this.showMore = true;
+        for (let i = 0; i < this.countRec; i++) {
+          if (i < 2) {
+            selectedRec.push(data[i]);
+          }
+        }
+        this.initialRecords = true;
+        this.userCredentialsData = selectedRec;
+        
+      } else {
+        this.initialRecords = true;
+        this.userCredentialsData = data;
+        
+      }
+
+      this.title = "Earned Credentials (" + data.length + ")";
+      this.loaded = true;
+    } else if (error) {
+      console.log("error1" + JSON.stringify(error));
+    }
+  }
+  connectedCallback() {
+    getCompletedUserCredentials({ userId: this.senddata })
+      .then((res) => {
+        this.totalUserCredentials = res;
+      })
+      .catch((error) => {
+        console.log("error2" + JSON.stringify(error));
+      });
+  }
+  showMoreRec() {
+    this.initialRecords = false;
+    this.showMoreRecords = true;
+    this.showMore = false;
+    this.userCredentialsData = this.totalUserCredentials;
+  }
+}
