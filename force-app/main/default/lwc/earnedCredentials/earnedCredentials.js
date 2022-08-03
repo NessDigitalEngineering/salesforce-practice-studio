1dc25be5ca635fe6119b505ea7f7855b9e18d8d8
Nitin Savanur <102808229+nitinsavanurness@users.noreply.github.com>
2022-08-03 22:40:06 +0530

    css changes

--- force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
+++ force-app/main/default/lwc/earnedCredentials/earnedCredentials.js
@@ -1,64 +1,33 @@
 import { LightningElement, wire, track, api } from "lwc";
 import getCompletedUserCredentials from "@salesforce/apex/UserCredentialService.getCompletedUserCredentials";
-import strUserId from "@salesforce/user/Id";
 import TasksIcon from "@salesforce/resourceUrl/EmptyCmpImage";
-import ShowmoreTitle from "@salesforce/label/c.CredentialTracking_ShowmoreTitle";
-import ShowlessTitle from "@salesforce/label/c.CredentialTracking_ShowlessTitle";
 import EmptyCard from "@salesforce/label/c.emptyCard";
+import Earned_Credentials from "@salesforce/label/c.Earned_Credentials";
 export default class EarnedCredentials extends LightningElement {
-	label = { ShowmoreTitle, ShowlessTitle, EmptyCard };
-	userIds = strUserId;
+	label = { EmptyCard, Earned_Credentials };
 	title;
 	userCredentialsData;
-	@track userCredData;
-	@track countRec;
-	@track showMore = false;
-	@track showLess = false;
-	@track initialRecords = false;
-	//@track showMoreRecords = false;
 	@track loaded = false;
-	@track totalUserCredentials;
 	@api senddata = "";
 	@track showIcon = true;
 	icon = TasksIcon;
-	@track showUtilityIcon = false;
 
 	@wire(getCompletedUserCredentials, { userId: "$senddata" }) userdata({ data, error }) {
 		if (data) {
-			this.totalUserCredentials = data;
-			this.countRec = data.length;
 			let selectedRec = [];
 
-			if (this.countRec > 2) {
-				this.showMore = true;
-				this.initialRecords = true;
-				for (let i = 0; i < this.countRec; i++) {
-					if (i < 2) {
-						selectedRec.push(data[i]);
-					}
-				}
-				this.userCredentialsData = selectedRec;
-				this.userCredData = selectedRec;
-			} else {
-				if (this.countRec === 0) {
-					this.showUtilityIcon = true;
-					//this.fireCountEvent("Earned Credentials (" + data.length + ")");
-					//this.title = "Earned Credentials (" + data.length + ")";
-				}
-				this.showMore = false;
-				this.initialRecords = true;
-				this.userCredentialsData = data;
+			for (let i = 0; i < data.length; i++) {
+				selectedRec.push(data[i]);
 			}
+			this.userCredentialsData = selectedRec;
 
-			if (this.countRec > 0) {
+
+			if (data.length > 0) {
 				this.showIcon = false;
-				this.fireCountEvent("Earned Credentials (" + data.length + ")");
-				//this.title = "Earned Credentials (" + data.length + ")";
+				this.fireCountEvent( Earned_Credentials +" (" + data.length + ")");
 			} else {
 				this.showIcon = true;
-				this.showUtilityIcon = false;
-				this.fireCountEvent("Earned Credentials");
-				//this.title = "Earned Credentials";
+				this.fireCountEvent(Earned_Credentials);
 			}
 
 			this.loaded = true;
@@ -75,36 +44,9 @@ export default class EarnedCredentials extends LightningElement {
 		this.dispatchEvent(countEvent);
 	}
 
-	connectedCallback() {
-		getCompletedUserCredentials({ userId: this.senddata })
-			.then((res) => {
-				this.totalUserCredentials = res;
-			})
-			.catch((error) => {
-				console.log("error2" + JSON.stringify(error));
-			});
-	}
-	showMoreRec() {
-		this.initialRecords = false;
-		this.showMoreRecords = true;
-		this.showMore = false;
-		this.showLess = true;
-		this.userCredentialsData = this.totalUserCredentials;
-	}
-
-	showLessRec() {
-		this.initialRecords = false;
-		this.showMoreRecords = false;
-		this.showMore = true;
-		this.showLess = false;
-		this.userCredentialsData = this.userCredData;
-	}
-
 	@api
 	resetCredentials() {
 	  this.userCredentialsData = [];
-	  this.showLess = false;
-	  this.showMore = false;
 	  this.showIcon = true;
 	}
 }
