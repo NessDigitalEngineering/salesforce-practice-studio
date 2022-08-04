d9e7d11fecd1512904e1e4794b858c1a13bc05f2
vidhyabalasubramaniamness <vidhya.balasubramaniam@ness.com>
2022-08-04 23:50:06 +0530

    voucher Request modals

--- force-app/main/default/lwc/credentialTracking/credentialTracking.js
+++ force-app/main/default/lwc/credentialTracking/credentialTracking.js
@@ -1,148 +1,167 @@
-import { LightningElement, api, wire, track } from 'lwc';
-import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
-import STATUS from '@salesforce/schema/User_Credential__c.Status__c';
-import getUserCredentials from '@salesforce/apex/CredentialTrackingController.getUserCredentials';
-import updateUserCredential from '@salesforce/apex/CredentialTrackingController.updateUserCredential';
-import USER_ID from '@salesforce/user/Id';
-import CompTitle from '@salesforce/label/c.CredentialTracking_Title';
-import ShowmoreTitle from '@salesforce/label/c.CredentialTracking_ShowmoreTitle';
-import ShowlessTitle from '@salesforce/label/c.CredentialTracking_ShowlessTitle';
+import { LightningElement, api, wire, track } from "lwc";
+import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
+import STATUS from "@salesforce/schema/User_Credential__c.Status__c";
+import getUserCredentials from "@salesforce/apex/CredentialTrackingController.getUserCredentials";
+import updateUserCredential from "@salesforce/apex/CredentialTrackingController.updateUserCredential";
+import USER_ID from "@salesforce/user/Id";
+import CompTitle from "@salesforce/label/c.CredentialTracking_Title";
+import ShowmoreTitle from "@salesforce/label/c.CredentialTracking_ShowmoreTitle";
+import ShowlessTitle from "@salesforce/label/c.CredentialTracking_ShowlessTitle";
 import TasksIcon from "@salesforce/resourceUrl/EmptyCmpImage";
 
 export default class CredentialTracking extends LightningElement {
- 
-    label = { CompTitle, ShowmoreTitle , ShowlessTitle};
-    userIds = USER_ID;
-    title;
-    Icn = TasksIcon;
-    userCredData;
-    statusValuesReady = false;
-    @track userCredentialsData;
-    @track statusValues = [];
-    @track countRec;
-    @track showMore = false;
-    @track showMre = false;
-    @track initialRecords = false;
-    @track showMoreRecords = false; 
-    @track showLess=false;
-    @track userCredData;
-    @track showIcon = false;
-    @track emptyRecords = true;
-    @wire(getObjectInfo, { objectApiName: 'User_Credential__c' })
-    userCredentialMetadata;
+	label = { CompTitle, ShowmoreTitle, ShowlessTitle };
+	userIds = USER_ID;
+	title;
+	Icn = TasksIcon;
+	userCredData;
+	statusValuesReady = false;
+	@track userCredentialsData;
+	@track statusValues = [];
+	@track countRec;
+	@track showMore = false;
+	@track showMre = false;
+	@track initialRecords = false;
+	@track showMoreRecords = false;
+	@track showLess = false;
+	@track userCredData;
+	@track showIcon = false;
+	@track emptyRecords = true;
+	@track showModal = false;
+	@track displayExamDetailsModal = false;
+	@wire(getObjectInfo, { objectApiName: "User_Credential__c" })
+	userCredentialMetadata;
 
-    @wire(getPicklistValues, { recordTypeId: '$userCredentialMetadata.data.defaultRecordTypeId', fieldApiName: STATUS })
-    picklistValues({ data, error }) {
-        
-        if (data) {
-            for (const statusVal of data.values) {
-                this.statusValues.push(statusVal.value);
-            }
-            this.statusValuesReady = true;
-            
-        }
-        if (error) {
-            console.log(error);
-        }
-    }
+	@wire(getPicklistValues, { recordTypeId: "$userCredentialMetadata.data.defaultRecordTypeId", fieldApiName: STATUS })
+	picklistValues({ data, error }) {
+		if (data) {
+			for (const statusVal of data.values) {
+				this.statusValues.push(statusVal.value);
+			}
+			this.statusValuesReady = true;
+		}
+		if (error) {
+			console.log(error);
+		}
+	}
 
-
-    connectedCallback() {
-        getUserCredentials({ userId: this.userIds })
-            .then((res) => {
-                this.totalUserCredentials = res;
-                this.processStatusValues();
-            })
-            .catch((error) => {
-                console.log("error" + JSON.stringify(error));
-            });
-    }
-    handleClick(event) {
-       
-        updateUserCredential({ id: event.target.value, status: event.target.title })
-            .then((result) => {
-                if (result === true) {
-                    getUserCredentials({ userId: this.userIds })
-                        .then((rs) => {
-                            this.totalUserCredentials = rs;
-                            this.processStatusValues();
-                        })
-                        .catch((error) => {
-                            console.log("error" + JSON.stringify(error));
-                        });
-                }
-            })
-            .catch((error) => {
-                console.log("error" + JSON.stringify(error));
-            });
-    }
-    processStatusValues() {
-        
-        
-        if (this.statusValuesReady) {
-            this.totalUserCredentials.forEach(e => {
-                if (e.Status__c && e.Status__c != 'Completed') {
-                    e.nextStatusLbl = this.statusValues[this.statusValues.indexOf('' + e.Status__c) + 1] + ' >';
-                    e.nextStatus = this.statusValues[this.statusValues.indexOf('' + e.Status__c) + 1];
-                    e.credentialName = e.Credential__r.Name;
-                }
-            });
-            this.processData(this.totalUserCredentials);
-        } else { 
-            this._interval = setInterval(() => {
-                this.processStatusValues();
-                clearInterval(this._interval);
-            }, 0);
-        }
-    }
-    processData(data) { 
-        this.totalUserCredentials = data;
-        this.countRec = data.length;
-        let selectedRec = [];
-        if (!this.showMoreRecords) {
-            if (this.countRec > 2) {
-                this.showMore = true;
-                this.initialRecords = true;
-                for (let i = 0; i < this.countRec; i++) {
-                    if (i < 2) {
-                        selectedRec.push(data[i]);
-                    }
-                }
-                this.userCredentialsData = selectedRec;
-                this.userCredData = selectedRec;
-            } else {
-                if (data.length === 0) {
-					this.showIcon = true;
-                    this.emptyRecords = false;
+	connectedCallback() {
+		getUserCredentials({ userId: this.userIds })
+			.then((res) => {
+				this.totalUserCredentials = res;
+				this.processStatusValues();
+			})
+			.catch((error) => {
+				console.log("error" + JSON.stringify(error));
+			});
+	}
+	handleClick(event) {
+		updateUserCredential({ id: event.target.value, status: event.target.title })
+			.then((result) => {
+				if (result === true) {
+					getUserCredentials({ userId: this.userIds })
+						.then((rs) => {
+							this.totalUserCredentials = rs;
+							this.processStatusValues();
+						})
+						.catch((error) => {
+							console.log("error" + JSON.stringify(error));
+						});
 				}
-                this.showMore = false;
-                this.initialRecords = true;
-                this.userCredentialsData = data;
-            }
-        } else {
-            this.userCredentialsData = data;
-        }
-        if(this.countRec > 0){
-            this.title = this.label.CompTitle + " (" + data.length + ")";
-        }else{
-            this.title = this.label.CompTitle;
-        }
-    }
-    showMoreRec() {
-        this.initialRecords = false;
-        this.showMoreRecords = true;
-        this.showMore = false;
-        this.showLess = true;
-        this.userCredentialsData = this.totalUserCredentials;
+			})
+			.catch((error) => {
+				console.log("error" + JSON.stringify(error));
+			});
+	}
+	processStatusValues() {
+		if (this.statusValuesReady) {
+			this.totalUserCredentials.forEach((e) => {
+				if (e.Status__c && e.Status__c != "Completed") {
+					if (e.Status__c != "Ready") {
+						// this.hideModalBox();
+						e.nextStatusLbl = this.statusValues[this.statusValues.indexOf("" + e.Status__c) + 1] + " >";
+						e.nextStatus = this.statusValues[this.statusValues.indexOf("" + e.Status__c) + 1];
+						e.credentialName = e.Credential__r.Name;
+					} else {
+						e.credentialName = e.Credential__r.Name;
+						this.voucherRequestModal();
+					}
+					// e.nextStatusLbl = this.statusValues[this.statusValues.indexOf("" + e.Status__c) + 1] + " >";
+					// e.nextStatus = this.statusValues[this.statusValues.indexOf("" + e.Status__c) + 1];
+					// e.credentialName = e.Credential__r.Name;
+				}
+			});
+			this.processData(this.totalUserCredentials);
+		} else {
+			this._interval = setInterval(() => {
+				this.processStatusValues();
+				clearInterval(this._interval);
+			}, 0);
+		}
+	}
+	processData(data) {
+		this.totalUserCredentials = data;
+		this.countRec = data.length;
+		let selectedRec = [];
+		if (!this.showMoreRecords) {
+			if (this.countRec > 2) {
+				this.showMore = true;
+				this.initialRecords = true;
+				for (let i = 0; i < this.countRec; i++) {
+					if (i < 2) {
+						selectedRec.push(data[i]);
+					}
+				}
+				this.userCredentialsData = selectedRec;
+				this.userCredData = selectedRec;
+			} else {
+				if (data.length === 0) {
+					this.showIcon = true;
+					this.emptyRecords = false;
+				}
+				this.showMore = false;
+				this.initialRecords = true;
+				this.userCredentialsData = data;
+			}
+		} else {
+			this.userCredentialsData = data;
+		}
+		if (this.countRec > 0) {
+			this.title = this.label.CompTitle + " (" + data.length + ")";
+		} else {
+			this.title = this.label.CompTitle;
+		}
+	}
 
-    }
+	showMoreRec() {
+		this.initialRecords = false;
+		this.showMoreRecords = true;
+		this.showMore = false;
+		this.showLess = true;
+		this.userCredentialsData = this.totalUserCredentials;
+	}
 
-    showLessRec() {
-        this.initialRecords = false;
-        this.showMoreRecords = false;
-        this.showMore = true;
-        this.showLess = false;
-        this.userCredentialsData = this.userCredData;
+	showLessRec() {
+		this.initialRecords = false;
+		this.showMoreRecords = false;
+		this.showMore = true;
+		this.showLess = false;
+		this.userCredentialsData = this.userCredData;
+	}
 
-    }
+	voucherRequestModal() {
+		this.showModal = true;
+	}
+
+	hideModalBox() {
+		this.showModal = false;
+	}
+
+	closeModal() {
+		this.displayExamDetailsModal = false;
+	}
+
+	showExamDetailsModal() {
+		this.displayExamDetailsModal = true;
+	}
 }
