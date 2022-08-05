253c65961aa3f6381d5a2e99d23718c7c0938887
pathiputturniranjanbabuness <110110108+pathiputturniranjanbabuness@users.noreply.github.com>
2022-08-05 17:59:35 +0530

    Niranjan credential branch (#102)
    Credential Tracking Changes

--- force-app/main/default/lwc/credentialTracking/credentialTracking.js
+++ force-app/main/default/lwc/credentialTracking/credentialTracking.js
@@ -5,32 +5,32 @@ import getUserCredentials from '@salesforce/apex/CredentialTrackingController.ge
 import updateUserCredential from '@salesforce/apex/CredentialTrackingController.updateUserCredential';
 import USER_ID from '@salesforce/user/Id';
 import CompTitle from '@salesforce/label/c.CredentialTracking_Title';
-import ShowmoreTitle from '@salesforce/label/c.CredentialTracking_ShowmoreTitle';
-import ShowlessTitle from '@salesforce/label/c.CredentialTracking_ShowlessTitle';
 import TasksIcon from "@salesforce/resourceUrl/EmptyCmpImage";
 
 export default class CredentialTracking extends LightningElement {
  
-    label = { CompTitle, ShowmoreTitle , ShowlessTitle};
+    label = { CompTitle};
     userIds = USER_ID;
     title;
     Icn = TasksIcon;
-    userCredData;
     statusValuesReady = false;
     @track userCredentialsData;
     @track statusValues = [];
-    @track countRec;
-    @track showMore = false;
-    @track showMre = false;
-    @track initialRecords = false;
-    @track showMoreRecords = false; 
-    @track showLess=false;
-    @track userCredData;
+    @track countRec; 
     @track showIcon = false;
     @track emptyRecords = true;
+
+    /*
+        @description    :   Wire service is used to get metadata for a single object 
+        @param          :   passing object(User_Credential__c) API name 
+    */
     @wire(getObjectInfo, { objectApiName: 'User_Credential__c' })
     userCredentialMetadata;
 
+    /*
+        @description    :   Using wire service to get all picklist values 
+        @param          :   defaultRecordTypeId & STATUS
+    */
     @wire(getPicklistValues, { recordTypeId: '$userCredentialMetadata.data.defaultRecordTypeId', fieldApiName: STATUS })
     picklistValues({ data, error }) {
         
@@ -46,7 +46,10 @@ export default class CredentialTracking extends LightningElement {
         }
     }
 
-
+    /*
+        @description    :   Displays logged IN user current assignments. 
+        @param          :   userIds
+    */
     connectedCallback() {
         getUserCredentials({ userId: this.userIds })
             .then((res) => {
@@ -57,6 +60,11 @@ export default class CredentialTracking extends LightningElement {
                 console.log("error" + JSON.stringify(error));
             });
     }
+
+    /*
+        @description    :   Displays all active user credential records when status value is not equal to completed 
+        @param          :   event target value & event target title
+    */
     handleClick(event) {
        
         updateUserCredential({ id: event.target.value, status: event.target.title })
@@ -76,6 +84,10 @@ export default class CredentialTracking extends LightningElement {
                 console.log("error" + JSON.stringify(error));
             });
     }
+
+    /*
+        @description    :   Update status value
+    */
     processStatusValues() {
         
         
@@ -95,54 +107,22 @@ export default class CredentialTracking extends LightningElement {
             }, 0);
         }
     }
+
+    /*
+        @description    :   Displays logged IN user current assignments and title, also shows the Icon if no records are found.
+        @param          :   data
+    */
     processData(data) { 
-        this.totalUserCredentials = data;
-        this.countRec = data.length;
-        let selectedRec = [];
-        if (!this.showMoreRecords) {
-            if (this.countRec > 2) {
-                this.showMore = true;
-                this.initialRecords = true;
-                for (let i = 0; i < this.countRec; i++) {
-                    if (i < 2) {
-                        selectedRec.push(data[i]);
-                    }
-                }
-                this.userCredentialsData = selectedRec;
-                this.userCredData = selectedRec;
-            } else {
-                if (data.length === 0) {
-					this.showIcon = true;
-                    this.emptyRecords = false;
-				}
-                this.showMore = false;
-                this.initialRecords = true;
-                this.userCredentialsData = data;
-            }
-        } else {
-            this.userCredentialsData = data;
+            this.countRec = data.length;
+        if (data.length === 0) {
+            this.showIcon = true;
+            this.emptyRecords = false;
         }
+            this.userCredentialsData = data;
         if(this.countRec > 0){
             this.title = this.label.CompTitle + " (" + data.length + ")";
         }else{
             this.title = this.label.CompTitle;
         }
-    }
-    showMoreRec() {
-        this.initialRecords = false;
-        this.showMoreRecords = true;
-        this.showMore = false;
-        this.showLess = true;
-        this.userCredentialsData = this.totalUserCredentials;
-
-    }
-
-    showLessRec() {
-        this.initialRecords = false;
-        this.showMoreRecords = false;
-        this.showMore = true;
-        this.showLess = false;
-        this.userCredentialsData = this.userCredData;
-
     }
 }
