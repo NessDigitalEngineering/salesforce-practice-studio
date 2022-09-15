b44f0da6647a05d308cd027231775a45de627257
Dipshika Nalawade <dipshika.nalawade@solarturbines.com>
2022-09-15 11:26:09 +0530

    updated class15 sep

--- force-app/main/default/lwc/credentialExamAttempts/credentialExamAttempts.js
+++ force-app/main/default/lwc/credentialExamAttempts/credentialExamAttempts.js
@@ -1,6 +1,7 @@
 import { LightningElement , api, track } from 'lwc';
 import getExamAttemptsForUser from "@salesforce/apex/credentialExamAttemptController.getExamAttempts";
 import updateStatus from "@salesforce/apex/credentialExamAttemptController.updateStatus";
+import updateDate from "@salesforce/apex/credentialExamAttemptController.updateDate";
 import USER_ID from "@salesforce/user/Id";
 import EXAM_DATE from "@salesforce/schema/Credential_Exam_Attempt__c.Exam_Date_Time__c";
 import Exam from "@salesforce/label/c.Exam";
@@ -10,18 +11,21 @@ export default class CredentialExamAttempts extends LightningElement {
     userIds = USER_ID;
     @track searchRecords;
     label = { Exam, ExamAttempt_EmptyMsg };
-	userIds = USER_ID;
-	title;
-	Icn = TasksIcon;
+    userIds = USER_ID;
+    title;
+    Icn = TasksIcon;
     @track userCredentialsData;
     @track countRec;
-	@track showIcon = false;
-	@track emptyRecords = true;
+    @track showIcon = false;
+    @track emptyRecords = true;
     @track editDate = false;
+    @track editExamDate = false;
+    @track exmDate;
+    @track credExamAttemptId;
     dt;
 
     connectedCallback() {
-		getExamAttemptsForUser({ userId: this.userIds })
+        getExamAttemptsForUser({ userId: this.userIds })
         .then((res) => {
             this.searchRecords = res;
             
@@ -30,11 +34,11 @@ export default class CredentialExamAttempts extends LightningElement {
                 this.showIcon = true;
                 this.emptyRecords = false;
             }
-		if (this.countRec > 0) {
-			this.title = this.label.Exam + " (" + res.length + ")";
-		} else {
-			this.title = this.label.Exam;
-		}
+        if (this.countRec > 0) {
+            this.title = this.label.Exam + " (" + res.length + ")";
+        } else {
+            this.title = this.label.Exam;
+        }
         })
         .catch((error) => {
             console.log("error" + JSON.stringify(error));
@@ -42,28 +46,23 @@ export default class CredentialExamAttempts extends LightningElement {
     }
 
     handleClick(event){
-        //alert('id'+event.target.value);
         updateStatus({examAttemptRecordId : event.target.value, examStatus: 'Exam Scheduled'}).then((response)=>{
             console.log('Record updated successfully');
-            
         }).catch((error) => {});
        
     }
     handleDateEdit() {
-        this.editDate = true;
+        this.editExamDate = true;
       }
-      handleDateChange(event) {
-        this.dt = event.target.value;
-      }
-    handleUpdate(event){
-        const fields = {};
-        fields[examAttemptRecordId] = event.target.value;
-        fields[EXAM_DATE.fieldApiName] = this.dt;
-        const recordInput = { fields };
-        updateDate(recordInput)
-        .then((response)=>{
-            this.editDate = false;
-        }).catch((error) => {});
-    }
 
+      updateExmDate(event){
+        this.credExamAttemptId = event.target.name;
+        this.exmDate = event.target.value;
+        updateDate({examAttemptRecordId : this.credExamAttemptId, dt: this.exmDate})
+        .then((res) => {
+            console.log('Record updated successfully');
+        })
+        .catch((error) => {});
+    }
+      
 }
